af5fe4ed50f1080b679d00b25bdb2678807b0627
Ocean Alves <oceansantos@yahoo.com>
2024-01-18 12:46:29 -0300

    Ajustes nos códigos.

--- desafio.kt
+++ desafio.kt
@@ -2,7 +2,7 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String, var formacao: Formacao? = null)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
@@ -11,11 +11,44 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        usuario.formacao = this
+        inscritos.add(usuario)
+        //println("${usuario.nome} foi matriculado na formação $nome.")
+                
     }
+    
+    fun listarInscritos() {
+        println("Usuários matriculados na formação $nome:")
+        inscritos.forEach { println("${it.nome} - ${it.formacao?.nome ?: "Não matriculado"}") }
+    }   
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Criar alguns conteúdos educacionais
+    val kotlinIntroducao = ConteudoEducacional("Introdução ao Kotlin", 60)
+    val kotlinEstruturas = ConteudoEducacional("Estruturas de Controle em Kotlin", 90)
+    val kotlinFuncoes    = ConteudoEducacional("Funções em Kotlin", 75)
+
+    // Criar uma formação
+    val formacaoKotlin = Formacao("Kotlin Básico",
+                                  listOf(
+                                      kotlinIntroducao,
+                                      kotlinEstruturas,
+                                      kotlinFuncoes)
+                                 )
+
+    // Criar alguns usuários
+    val usuario1 = Usuario("João")
+    val usuario2 = Usuario("Maria")
+    val usuario3 = Usuario("Bia")
+
+    // Matricular os usuários na formação
+    formacaoKotlin.matricular(usuario1)
+    formacaoKotlin.matricular(usuario2)
+    formacaoKotlin.matricular(usuario3)
+    
+    //formacaoKotlin.listarInscritos()
+    // Verificar os inscritos na formação
+    formacaoKotlin.listarInscritos()
+
 }
